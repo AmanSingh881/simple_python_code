acb298fbc4c339e981218b867f21efae0536bd55
Aman <102499131+Adhikari881@users.noreply.github.com>
2023-09-19 12:33:43 +0530

    Create simple.py

---
+++ simple.py
@@ -0,0 +1,9 @@
+import time
+
+output_file = '/path/to/timestamps.txt'
+
+while True:
+    current_time = time.strftime('%Y-%m-%d %H:%M:%S')
+    with open(output_file, 'a') as file:
+        file.write(current_time + '\n')
+    time.sleep(1)
